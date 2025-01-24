ab2684abb2ebf320d6f1400634cb0520a68e6506
Zuzanna Kosek <zkosek@student.agh.edu.pl>
2025-01-24 19:53:18 +0100

    "Kodowi Magowie" - gra

---
+++ game.py
@@ -0,0 +1,131 @@
+print("Witaj w grze Kodowi Magowie!!")
+x = input("Czy chcesz zacząć nową grę? T/N ")
+if x == 'T' or x == 't':
+  print(" *karczma jest hałaśliwa i pełna ludzi. Gothard siedząc przy barze z nowym karczmarzem, zaczyna opowiadać swoją historię.* ")
+  print("")
+  print("Gothard: Ach, karczmarzu... Kiedyś byłem kimś. Wielkim magiem, rozumiesz? Ludzie mnie szanowali.")
+  print("Ale to wszystko przepadło. Wojna... Chaos. Straciłem dom, przyjaciół, a teraz... Zosię. *Wzdycha* Mówią, że nie można przegrać wszystkiego, ale ja udowodniłem, że się da.")
+  print("")
+  print("Karczmarz: Cóż, Gothardzie, życie to ciężka bestia. Ale zawsze można zaczać od nowa. Co powiesz na jeszcze jedno piwo?")
+  print("")
+
+  piwo = input("A. Pewnie.   B.Nie no, już za dużo wypiłem. Trzeba znać umiar. Wróce jutro B) ")
+  if piwo == 'A' or piwo == 'a':
+    print("")
+    print("Gothard: Niech będzie. Jeszcze jeden kufel za stare czasy!")
+    print("")
+    print(" *Gothard wypija piwo, czuje się nieco pijany* ")
+    print("")
+    print("Karczmarz: Rozsądnie. Powodzenia, magu. Może los się do ciebie uśmiechnie.")
+
+    print("")
+    print(" *Gothard wychodzi z karczmy. W ciemnym zaułku napotyka grupę bandytów* ")
+    print("")
+    print("Bandyta 1: Oho, patrzcie, maguś się nam trafił! Może masz coś, co nas zainteresuje, hę?")
+    print("")
+    print("Gothard: Lepiej idźcie swoją drogą, bo pożałujecie.")
+    print("")
+    print("Bandyta 2: No proszę, grozi nam! Chłopaki, czas pokazać temu czarodziejowi, kto tu rządzi!")
+    
+
+    ## TUTAJ KIEDYŚ BĘDZIE KOD
+
+    # jeśli przegrasz
+
+    print(" *Bandyci śmieją się, a Gothard ginie.* ")
+    print("KONIEC GRY")
+    print("DZIĘKUJEMY :D ~ ekipa kodowych magów")
+
+    ## Po zwycięstwie:
+
+    print(" *Gothard stoi nad pokonanymi bandytami, oddycha ciężko* ")
+    print("")
+    print("Gothard: Ha! Mówiłem, że pożałujecie *Patrzy na horyzont* Czas wrócić do mojego poszukiwania. Zosia, gdzie ty jesteś...")
+    print("")
+
+
+    print(" *Gothard idzie przez las i nagle widzi błyszczącą kulę. W niej pojawia się obraz Zosi, zamkniętej w wieży* ")
+    print("")
+    print("Gothard: Zosia?! Co… Co to za wieża? *Przygląda się bliżej* Ten symbol… To musi być dzieło jakiegoś maga chaosu. Niech to szlag! Muszę coś zrobić.")
+    print("")
+    
+    ##Gracz wybiera: iść od razu do wieży lub szukać krasnoluda Mirka.
+
+    #TAK
+
+    print(" *Gothard znajduje krasnoluda Mirka w jego kuźni* ")
+    print("")
+    print("Gothard: Mirek, potrzebuję twojej pomocy. Zosia została porwana przez maga chaosu. Muszę ją uratować.")
+    print("")
+    print("Mirek: Mag chaosu, powiadasz? A co ja będę z tego miał?")
+
+    #Gracz musi rozwiązać zagadkę logiczną, aby przekonać Mirka.
+
+    #jesli sie uda (jak sie nie uda to to samo jakbys nie szedl po mirka wcale)
+    print("")
+    print("Mirek: Dobra, idę z tobą, stary druhu. Razem damy radę!")
+    print("")
+    
+
+    #wieża z mirkiem
+    print(" *Mirek i Gothard przemierzają podziemne tunele pełne pułapek i potworów* ")
+    print("")
+    print("Mirek: Tunelami dotrzemy do lochów. Ale lepiej uważaj, bo te korytarze śmierdzą starą magią.")
+    print("")
+    print(" *W lochach znajdują więźnia.* ")
+    print("")
+    print("Więzień: Proszę, uwolnijcie mnie! Powiem wam, jak pokonać maga chaosu.")
+    print("")
+
+    print("Gothard: Mów.")
+    print("Więzień: Jest zakazane zaklęcie, które może go zniszczyć. Ale użycie go rozprzestrzeni chaos. Pomyślcie dwa razy, zanim je rzucicie.")
+    print("")
+
+    #jesli bez mirka
+
+    print(" *Gothard teleportuje się przez magiczne drzwi, gdzie natychmiast atakują go strażnicy* ")
+    print("")
+    print("Gothard: Nie ma czasu na ceregiele. Przepuście mnie albo zginiecie!")
+    ##  WALKA ZE STRAŻNIKAMI
+
+
+    print("*Gothard wchodzi do sali tronowej. Mag chaosu stoi na podwyższeniu, trzyma Zosię w magicznych kajdanach*")
+    print("")
+    print("Mag Chaosu: O, przybyłeś, Gothardzie. Idealnie. Będziesz świadkiem narodzin nowego ładu!")
+    print("")
+    print("Gothard: Wypuść ją! Natychmiast!")
+    print("")
+    print("Mag Chaosu: Albo co?")
+    print("")
+
+    #rozpoczyna się walka
+
+    # 1. tradycyjna walka
+    #jesli wygrasz
+    print(" *Mag chaosu ginie, Zosia zostaje uwolniona* ")
+    print("")
+    print("Zosia: Gothardzie, udało się! Dziękuję ci!")
+    print("")
+    print("Gothard: To jeszcze nie koniec, ale jesteś bezpieczna. To się liczy.")
+    print("")
+    print("KONIEC GRY - DOBRY ENDING.")
+    print("DZIĘKUJEMY")
+    print("")
+
+    # zginąć w walce
+    print(" *Mag chaosu triumfuje, Zosia pozostaje uwięziona* ")
+    print("")
+    print("KONIEC GRY - ZŁY ENDING.")
+    print("DZIĘKUJEMY")
+    print("")
+
+    #uzycie zakazanego zaklecia
+    print(" *Mag chaosu zostaje zniszczony, ale Zosia zostaje pochłonięta przez chaos.* ")
+    print("")
+    print("Gothard: Nie... Zosia... Co ja zrobiłem...")
+    print("")
+    print("KONIEC GRY - TRAGICZNY ENDING.")
+    print("DZIĘKUJEMY")
+    
+
+  
